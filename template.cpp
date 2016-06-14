8d45611c878111bf6575fa8887044ab1c1cc2076
Prakhar Pratyush <pnation2020@gmail.com>
2016-06-15 01:16:37 +0530

    updated

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cassert>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+define ll long long int
+
+int main(){
+    ios_base::sync_with_stdio(false); //now cin/cout same speed as scanf/printf
+    
+    return 0;
+    }
